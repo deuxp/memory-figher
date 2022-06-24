8af093a0b071a1bf323abba3f06a832c250f53ec
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-06-24 12:08:22 -0400

    File Structure: tidy up the start

---
+++ src/Components/Title/Title.tsx
@@ -0,0 +1,9 @@
+// import style from "Title.module.scss";
+
+function Title() {
+  // const { title } = style;
+  return <div>Aa</div>;
+  // return <div className={title}>Aa</div>;
+}
+
+export default Title;
